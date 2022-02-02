ff927cc344f0e9e2cd3ba1f0e9da094422710f2f
KwaneleN02 <54561549+KwaneleN02@users.noreply.github.com>
2022-02-02 09:56:43 -0700

    SRC

---
+++ src/app/server/admin.component.ts
@@ -0,0 +1,17 @@
+import { Component, NgModule } from "@angular/core";
+
+@Component
+(
+    {
+        //This tells Angular what to do with this class. 
+        selector: 'app-admin', 
+        templateUrl: './admin.component.html',
+        styleUrls:['./admin.component.css']
+    } 
+
+)
+
+export class AdminComponent
+{
+
+}
